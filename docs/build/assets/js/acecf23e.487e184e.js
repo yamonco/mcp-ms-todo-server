8bac59cc533529b8e064b8d10b817a98baaac11b
JongdaeKim <battlecruser@yamon.io>
2025-09-03 07:23:08 +0900

    Fix: set baseUrl to /mcp-ms-todo-server/ for GitHub Pages and allow build directory

---
+++ docs/build/assets/js/acecf23e.487e184e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/mcp-ms-todo-server/blog","blogTitle":"Blog","authorsListPath":"/mcp-ms-todo-server/blog/authors"}')}}]);
